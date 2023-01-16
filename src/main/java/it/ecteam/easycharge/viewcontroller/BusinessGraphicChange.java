fe1ba727af2e3ab056a1e359f8811e0deb123be6
Simone <45819378+simonebucci@users.noreply.github.com>
2023-01-16 19:31:36 +0100

    minor fixes

--- src/main/java/it/ecteam/easycharge/viewcontroller/BusinessGraphicChange.java
+++ src/main/java/it/ecteam/easycharge/viewcontroller/BusinessGraphicChange.java
@@ -6,13 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.function.Supplier;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class BusinessGraphicChange extends GraphicChangeTemplate{
     private static BusinessGraphicChange myInstance=null;
-    protected static final Logger logger = Logger.getLogger("gc");
 
     private BusinessGraphicChange() {
         whoAmI = Roles.BUSINESS;
@@ -31,7 +28,7 @@ public class BusinessGraphicChange extends GraphicChangeTemplate{
             Scene scene = new Scene(loader.load(), stage.getScene().getWidth(), stage.getScene().getHeight());
             stage.setScene(scene);
         } catch (IOException e) {
-            logger.log(Level.WARNING, (Supplier<String>) e);
+            logger.log(Level.WARNING, e.toString());
         }
     }
 
--- src/main/java/it/ecteam/easycharge/viewcontroller/UserGraphicChange.java
+++ src/main/java/it/ecteam/easycharge/viewcontroller/UserGraphicChange.java
@@ -7,13 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.function.Supplier;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class UserGraphicChange extends GraphicChangeTemplate {
     private static UserGraphicChange myInstance=null;
-    protected static final Logger logger = Logger.getLogger("gc");
 
     private UserGraphicChange() {
         whoAmI = Roles.USER;
@@ -32,7 +29,7 @@ public class UserGraphicChange extends GraphicChangeTemplate {
             Scene scene = new Scene(loader.load(), stage.getScene().getWidth(), stage.getScene().getHeight());
             stage.setScene(scene);
         } catch (IOException e) {
-            logger.log(Level.WARNING, (Supplier<String>) e);
+            logger.log(Level.WARNING, e.toString());
         }
     }
 
