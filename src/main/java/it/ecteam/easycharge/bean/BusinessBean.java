d3b099f75657f7a912e4ff771540969d67885408
Simone <45819378+simonebucci@users.noreply.github.com>
2023-02-08 00:48:43 +0100

    Update BusinessBean.java

--- src/main/java/it/ecteam/easycharge/bean/BusinessBean.java
+++ src/main/java/it/ecteam/easycharge/bean/BusinessBean.java
@@ -11,61 +11,33 @@ public class BusinessBean {
     protected String id;
     protected String ad;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return this.username;
-    }
+    public String getUsername() { return this.username; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getPassword() {
-        return this.password;
-    }
+    public String getPassword() { return this.password; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return this.email;
-    }
+    public String getEmail() { return this.email; }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+    public void setRole(String role) { this.role = role; }
 
-    public String getRole() {
-        return this.role;
-    }
+    public String getRole() { return this.role; }
 
-    public void setBusiness(String business) {
-        this.business = business;
-    }
+    public void setBusiness(String business) { this.business = business; }
 
-    public String getBusiness() {
-        return this.business;
-    }
+    public String getBusiness() { return this.business; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public void setAddress(String address) { this.address = address; }
 
-    public String getAddress() {
-        return this.address;
-    }
+    public String getAddress() { return this.address; }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public void setId(String id) { this.id = id; }
 
-    public String getId() {
-        return this.id;
-    }
+    public String getId() { return this.id; }
     public String getAd(){ return this.ad; }
     public void setAd(String ad){ this.ad = ad;}
 }
